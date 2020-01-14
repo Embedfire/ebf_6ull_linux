fe1b9b8fe423aa41ff53757b94adddea8973662d
jiejie <1161959934@qq.com>
2020-01-14 08:06:54 +0000

    update config

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Tue Jan 14 07:06:51 UTC 2020"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jan 14 08:05:48 UTC 2020"
 #define LINUX_COMPILE_BY "jiejie"
 #define LINUX_COMPILE_HOST "embedfire_dev"
 #define LINUX_COMPILER "gcc version 4.9.3 20141031 (prerelease) (Linaro GCC 2014.11) "
